1a73d8f4a1f3703143fc1654f70db980c574d059
zzhtttsss <634879238@qq.com>
2022-12-17 20:41:53 +0800

    feat(storer): support grpc.

---
+++ connect/src/main/java/org/catmq/App.java
@@ -0,0 +1,13 @@
+package org.catmq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
