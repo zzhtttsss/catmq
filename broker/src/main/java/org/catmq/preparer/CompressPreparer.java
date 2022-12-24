cbdae1fb2872d92e48408991d043b0cfbb012132
zzhtttsss <634879238@qq.com>
2022-12-24 14:06:14 +0800

    feat(broker): build broker architecture.

--- broker/src/main/java/org/catmq/preparer/CompressPreparer.java
+++ broker/src/main/java/org/catmq/preparer/CompressPreparer.java
@@ -1,13 +1,15 @@
 package org.catmq.preparer;
 
 import org.catmq.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CompressPreparer implements Preparer {
-
+    Logger logger = LoggerFactory.getLogger(CompressPreparer.class);
     public static final String COMPRESS_PREPARER = "CompressPreparer";
 
     public RequestContext prepare(RequestContext ctx) {
-        ctx.testField = ctx.testField + " CompressPreparer";
+        logger.info("CompressPreparer");
 
         return ctx;
     }
