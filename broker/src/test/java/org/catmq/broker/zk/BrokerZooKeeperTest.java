ecdaf4775bd68340fd3902167dc515a9a1a31a75
DividedMoon <yidian20000105@163.com>
2022-12-31 15:17:04 +0800

    feature(zookeeper):fix bugs

--- broker/src/test/java/org/catmq/broker/zk/BrokerZooKeeperTest.java
+++ broker/src/test/java/org/catmq/broker/zk/BrokerZooKeeperTest.java
@@ -3,12 +3,13 @@ package org.catmq.broker.zk;
 import org.apache.curator.test.TestingServer;
 import org.catmq.Broker;
 import org.catmq.broker.BrokerInfo;
+import org.catmq.command.BooleanError;
 import org.catmq.zk.BrokerZooKeeper;
+import org.catmq.zk.balance.ILoadBalance;
 import org.junit.*;
 import org.mockito.Mockito;
 
 import java.io.IOException;
-import java.util.List;
 
 @FixMethodOrder(org.junit.runners.MethodSorters.NAME_ASCENDING)
 public class BrokerZooKeeperTest {
@@ -26,20 +27,16 @@ public class BrokerZooKeeperTest {
         bzk.register2Zk();
         Assert.assertNotNull(bzk.client.checkExists().forPath("/broker/" + bzk.getBroker().brokerInfo.getBrokerName()));
         Assert.assertNotNull(bzk.client.checkExists().forPath("/broker/tmp/" + bzk.getBroker().brokerInfo.getBrokerName()));
-        List<String> strings = bzk.client.getChildren().forPath("/address/broker");
-        Assert.assertEquals(1, strings.size());
-        Assert.assertEquals(bzk.getBroker().brokerInfo.getBrokerIp() + ":" + bzk.getBroker().brokerInfo.getBrokerPort(),
-                strings.get(0));
-        String path = "/address/broker/" + strings.get(0);
-        Assert.assertEquals("0", new String(bzk.client.getData().forPath(path)));
         bzk.close();
     }
 
 
     private BrokerZooKeeper createFakeZk(BrokerInfo info) throws IOException {
         Broker broker = Mockito.mock(Broker.class);
+        ILoadBalance loadBalance = Mockito.mock(ILoadBalance.class);
+        Mockito.when(loadBalance.registerConnection(info)).thenReturn(BooleanError.ok());
         broker.brokerInfo = info;
-        return new BrokerZooKeeper(server.getConnectString(), broker, null);
+        return new BrokerZooKeeper(server.getConnectString(), broker, loadBalance);
     }
 
     @AfterClass
