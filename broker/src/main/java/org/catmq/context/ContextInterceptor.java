01a8252bbecba51c6106e732fe09113c23bcac02
zzhtttsss <51556543+zzhtttsss@users.noreply.github.com>
2023-01-19 16:37:30 +0800

    Revert "feat(storer): finish the write process." (#10)

---
+++ broker/src/main/java/org/catmq/context/ContextInterceptor.java
@@ -0,0 +1,20 @@
+package org.catmq.context;
+
+import io.grpc.*;
+import io.grpc.Context;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ContextInterceptor implements ServerInterceptor {
+
+    @Override
+    public <R, W> ServerCall.Listener<R> interceptCall(
+            ServerCall<R, W> call,
+            Metadata headers,
+            ServerCallHandler<R, W> next
+    ) {
+        log.info(headers.toString());
+        io.grpc.Context context = Context.current().withValue(InterceptorConstants.METADATA, headers);
+        return Contexts.interceptCall(context, call, headers, next);
+    }
+}
--- storer/src/main/java/org/catmq/storage/segment/ReadCache.java
+++
@@ -1,4 +0,0 @@
-package org.catmq.storage.segment;
-
-public class ReadCache {
-}
